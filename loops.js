0db8510ce458592d189ff0e53ee1a245b14c1f75
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-18 19:24:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(array) {
       array.push("I am 1 strange loop.")
     }
     else if (i > 1){
-      array.push("I am" + ${i}  "strange loops.")
+      array.push("I am ${i} strange loops.")
     }
   }
 }
